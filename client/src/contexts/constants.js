2b3266a2f154b6001f4fb6ca3d1162e56861fb63
NguyenLam05200 <holohoi05200@gmail.com>
2021-11-12 17:06:39 +0700

    Prepare deploy

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,2 +1,2 @@
-export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : 'somedeployedURL'
+export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : 'https://boiling-reef-82876.herokuapp.com/api'
 export const LOCAL_STORAGE_TOKEN_NAME = 'classroom'
